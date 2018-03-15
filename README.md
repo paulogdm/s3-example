d704cfc3d31fc89a604ee03273fa382e96b0cae7
paulogdm <paulogdemitri@gmail.com>
2018-03-15 13:23:45 -0300

    First draft

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-micro-s3-example
+# Micro with S3 bucket example.
+
+Simple example using Zeit's micro and the AWS SDK to upload files to the cloud.
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+## How to use
+
+### Getting Started
+
+Clone this [repo](https://github.com/paulogdm/micro-s3-example):
+
+Edit the index.js:
+
+```javascript
+const BUCKET_NAME = 'cultofthepartyparrot'
+const ACCESS_KEY = 'MY_ACCESS_KEY'
+const SECRET_KEY = 'MY_SECRET_KEY'
+const REGION = 'us-west-1'
+```
+### Running and installing
+
+```bash
+npm install
+npm install -g micro-dev micro
+npm run start
+```
+
+### Using
+
+Upload file:
+
+```bash
+curl  --request POST --data-binary "./1.png" localhost:3000/1.png
+```
+
+Fetch file:
+
+```bash
+curl  --request GET localhost:3000/1.png
+```
+
+### Rest Clients
+
+You can also see this example action with [Insomnia](https://insomnia.rest/) importing the request from [insomnia.json](insomnia.json).
+
+A "HTTP Archive" file is also provided [here](HttpArchive.har).
+
+### Deploying to `now.sh`
+
+Deploy it to the cloud with [now](https://zeit.co/now)
+
+```bash
+now
+```
+
+## Note
+
+This example uploads the file to a server and then save it to S3.
+However you may want to just upload from the client directly to the S3 bucket with [Pre Signed Post](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#createPresignedPost-property)
+
+## Packages Used In This Example
+
+ * [micro](https://github.com/zeit/micro)
+ * [AWS SDK](https://github.com/aws/aws-sdk-js)
+ * [microrouter](https://github.com/pedronauck/micro-router)
+
+## Author
+
+[@paulogdm](https://github.com/paulogdm)
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,131 @@
+// Micro deps
+const { router, post, get } = require('microrouter')
+const { buffer, createError, send } = require('micro')
+
+// Configs
+const BUCKET_NAME = 'cultofthepartyparrot'
+const ACCESS_KEY = 'MY_ACCESS_KEY'
+const SECRET_KEY = 'MY_SECRET_KEY'
+const REGION = 'us-west-1'
+
+// AWS
+const aws = require('aws-sdk')
+aws.config.update({
+  'accessKeyId': ACCESS_KEY,
+  'secretAccessKey': SECRET_KEY,
+  'region': 'us-west-1',
+  'bucketname': BUCKET_NAME
+})
+
+// Set AWS to use native promises
+aws.config.setPromisesDependency(null)
+
+// New S3 class
+const s3 = new aws.S3()
+
+/**
+ * Uploads a buffer to the AWS
+ * @param  {Buffer} fileBuffer  File binary.
+ * @param  {String} contentType Type of content, usually the header of http request.
+ * @param  {String} s3key       File key to S3 bucket.
+ * @return {Promise}            Promise to be resolved or rejected.
+ */
+const uploadBuffer = async (fileBuffer, contentType, s3key) => {
+  return s3.putObject({
+    ACL: 'private',
+    Bucket: BUCKET_NAME,
+    Key: s3key,
+    Body: fileBuffer,
+    ContentType: contentType
+  }).promise()
+}
+
+/**
+ * Get file binary and `content-type`.
+ * Then uploads it to AWS.
+ * @param  {Object} req
+ * @param  {Object} res
+ * @return {String}     filename
+ */
+const set = async (req, res) => {
+  const buf = await buffer(req, {limit: '5mb'})
+  const filename = req.params.name + '.' + req.params.type
+  const contentType = req.headers['content-type']
+
+  if (!req.params.name || !req.params.type) {
+    throw createError(400, 'Bad Params')
+  }
+
+  await uploadBuffer(buf, contentType, filename)
+
+  return filename
+}
+
+/**
+ * Redirect a request to a given location.
+ * @param  {Object} req
+ * @param  {Object} res
+ * @param  {String} location Redirect URI
+ */
+const redirect = async (req, res, location) => {
+  res.setHeader('Location', location)
+  res.statusCode = 302
+  res.end()
+}
+
+/**
+ * Intercepts a request and try to fetch it from S3.
+ * ES6 Proxy could be very useful here.
+ * @param  {Object} req
+ * @param  {Object} res
+ */
+const fetch = async (req, res) => {
+  const filename = req.params.name + '.' + req.params.type
+
+  try {
+    await s3.headObject({
+      Bucket: BUCKET_NAME,
+      Key: filename
+    }).promise()
+
+    const uri = s3.getSignedUrl('getObject', {
+      Bucket: BUCKET_NAME,
+      Key: filename
+    })
+
+    return redirect(req, res, uri)
+  } catch (err) {
+    if (err.statusCode === 404) {
+      throw createError(404, 'Not Found')
+    } else throw err
+  }
+}
+
+/**
+ * Handle our errors nicely.
+ * @param  {Function} fn function to try-catch
+ */
+const handleErrors = fn => async (req, res) => {
+  try {
+    return await fn(req, res)
+  } catch (err) {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    console.error(err)
+    return send(res, statusCode, message)
+  }
+}
+
+/**
+ * Routes for this API.
+ * Isn't `microrouter` great?
+ */
+const routes = router(
+  get('/:name.:type', handleErrors(fetch)),
+  post('/:name.:type', handleErrors(set))
+)
+
+/**
+ * Exporting http server created by `microrouter`
+ */
+module.exports = routes
